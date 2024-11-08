7f9cef3bdfee890318ce74469ecbc7f7f44f248d
termuxer <131900623+termuxBash@users.noreply.github.com>
2024-11-08 16:46:24 +0530

    Add files via upload

--- HTML_Pages/Covid/Covid-data/Covid-data.js
+++ HTML_Pages/Covid/Covid-data/Covid-data.js
@@ -9,10 +9,8 @@ noBack=()=>setTimeout(noFront,400);
 function chkData(){
 if(sessionStorage.getItem("data")=="true"){type();}
 else{swal("No Data to display","No data was sent by the main website.","warning");}}
-function type(){
+function type(mainForm){
 const item=["num-value","time","queries","feel","fruit","color","email","suggest","quality","like"];
-var mainForm=JSON.parse(sessionStorage.getItem("mainForm"))
-//alert(JSON.stringify(mainForm))
 $("#name").html(mainForm.Name),
 $('input[name="cards"][value="'+mainForm.cards+'"]').prop("checked",true),
 $("h2").html(mainForm.yn);
@@ -24,9 +22,9 @@ $("#feel").html(mainForm.feel),
 $("#fruits").html(mainForm.fruit);
 $("#color").css("background-color",mainForm.color);
 $("#email").html(mainForm.email).attr("href","mailto:"+mainForm.email),
-$("#suggest").html(sessionStorage.getItem("suggest")),
-$("#like").html(sessionStorage.getItem("like")),
-$("#feedback").html(sessionStorage.getItem("quality"));
+$("#suggest").html(mainForm.suggest),
+$("#like").html(mainForm.like),
+$("#feedback").html(mainForm.quality);
 for(var i=0;i<item.length;i++){
 sessionStorage.removeItem(item[i]);}
 }
